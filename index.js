cd021d1d28977266267ce6874b600999a70ba754
sudeyuksek <60464361+sudeyuksek@users.noreply.github.com>
2022-04-08 01:09:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿const btn = document.getElementById('btn theme');
+
+//eğer tema değiştirme butonuna basıldıysa hakkımda bloğu,eğitim bloğu ve iletişim bloğu renk değiştirmektedir. 
+btn.addEventListener('click', function onClick(event) {
+    const box = document.getElementById('about');
+    const box_2 = document.getElementById('education');
+    const box_3 = document.getElementById('contact');
+
+    box.style.backgroundColor = 'lightslategrey';
+    box_2.style.backgroundColor = 'grey';
+    box_3.style.backgroundColor = 'lightslategrey';
+    box_3.style.color = 'snow';
+  
+
+});
